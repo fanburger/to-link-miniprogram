9db75ff46548de35129e5da866176f0d0d7d83b7
BM_YF <2922265540@qq.com>
2022-03-31 22:19:10 +0800

    🎉: 完成微信一键登录功能

--- api/user.js
+++ api/user.js
@@ -3,8 +3,10 @@ const {
 } = getApp().globalData
 const userLoginUrl = host + '/user/phone_login'
 const userSignUpUrl = host + '/user/signup'
+const userWxCodeLogin = host + '/user/wx_code_login'
 
 module.exports = {
   userLoginUrl,
-  userSignUpUrl
+  userSignUpUrl,
+  userWxCodeLogin
 }
